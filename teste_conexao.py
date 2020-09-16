e2527b52491dd51b63e77c16e3213ab511871db6
diegoPaladino <diegopaladinofoto@gmail.com>
2020-09-15 21:39:22 -0300

    202009152139

--- teste_conexao.py
+++ teste_conexao.py
@@ -4,14 +4,15 @@
 from speedtest import Speedtest
 import schedule
 import time
+from datetime import datetime
 
 #declarations
 st = Speedtest()
 # resultado_1 = st.download()/1000000
 
 def job():
-    print('Velocidade de Download:',st.download()/1000000)
-    print('Velocidade de Upload:',st.upload()/1000000)
+    print('Velocidade de Download:',st.download()/1000000,datetime.now())
+    print('Velocidade de Upload:',st.upload()/1000000,datetime.now())
 
 
 #execution
